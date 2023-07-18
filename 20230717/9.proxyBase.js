68aee852b51668de6ecc3477edae5bc087902152
王旭东 <xudong@DESKTOP-DK31HTA>
2023-07-18 19:49:41 +0800

    '垃圾回收机制尝试'

--- 20230717/9.proxyBase.js
+++ 20230717/9.proxyBase.js
@@ -13,7 +13,7 @@ const target = {
 //     console.log(receiver)
 //     return targetObj[queryProperty]
 // }
-// }
+// }  
 // 使用Reflect 反射创建空代理
 // const handler={
 //     get(){
@@ -25,19 +25,20 @@ const target = {
 // const proxy = new Proxy(target,Reflect)
 
 // 最终的代码
-// const handler ={
-//     get(targetObj,queryProperty,receiver){
-//         let decoration =''
-//         if(queryProperty ==='foo'){
-//             decoration="???"
-//         }
-//         return Reflect.get(...arguments)+decoration
-//     }
-// }
-// const proxy = new Proxy(target,handler)
-// console.log(target.foo)
-// console.log(proxy,Reflect)
-// console.log(proxy.foo)
+const handler ={
+    get(targetObj,queryProperty,receiver){
+        console.log(targetObj,queryProperty,receiver,666)
+        let decoration =''
+        if(queryProperty ==='foo'){
+            decoration="???"
+        }
+        return Reflect.get(...arguments)+decoration
+    }
+}
+const proxy = new Proxy(target,handler)
+console.log(target.foo,1)
+console.log(proxy,Reflect,2)
+console.log(proxy.foo,3)
  
 // 9.1.4 捕获器不变式
 // const target ={
@@ -86,24 +87,46 @@ const o = {}
 // }
 
 // 代理另一个代理
-const firstProxy = new Proxy(target,{
-    set(){
-  Object.defineProperty(target,'name',{
-    value:'wang'
-  })
-    },
-    get(){
-        console.log('first proxy',arguments)
-        return Reflect.get
-    }
-})
-const secondProxy = new Proxy(firstProxy,{
-    get(){
-        console.log('second proxy',arguments)
-        return Reflect.get
-    }
-})
+// const firstProxy = new Proxy(target,{
+//     set(){
+//   Object.defineProperty(target,'name',{
+//     value:'wang'
+//   })
+//     },
+//     get:Reflect.get
+// })
+// const secondProxy = new Proxy(firstProxy,{
+//     get: Reflect.get 
+// })
 
-const {proxy,revoke} = Proxy.revocable(target,secondProxy)
-revoke()
-console.log(proxy,firstProxy,secondProxy)
+// const {proxy,revoke} = Proxy.revocable(target,secondProxy)
+// revoke()
+// console.log(proxy,firstProxy,secondProxy)
+
+
+// console.log(target.thisValueEqualsProxy())
+// console.log(firstProxy.thisValueEqualsProxy(),secondProxy.thisValueEqualsProxy(),'------')
+
+// const wm = new WeakMap()
+// class User{
+//     constructor(userId){
+//         wm.set(this,userId)
+//     }
+//     set id(userId){
+//         wm.set(this,userId)
+//     }
+//     get id(){
+//         return wm.get(this)
+//     }
+// }
+// const user = new User(123,456,789)
+// console.log(user,wm)
+// const userInstanceProxy = new Proxy(user,{})
+// console.log(userInstanceProxy)
+
+// const dateTarget = new Date()
+// const proxy =new Proxy(target,{})
+// console.log(proxy instanceof Date)
+// proxy.getDate()
+
+ 
