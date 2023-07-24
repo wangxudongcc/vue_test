836835e7e245d6e143a6490ece9de43423aca531
王旭东 <xudong@DESKTOP-DK31HTA>
2023-07-24 18:02:41 +0800

    'proxyaddproperty...'

--- 20230717/9.2proxy&Reflect.js
+++ 20230717/9.2proxy&Reflect.js
@@ -1,4 +1,3 @@
- 
 const myTarget = {};
 // const proxy = new Proxy(myTarget, {
 //   get(target, property, receiver) {
@@ -16,7 +15,7 @@ const myTarget = {};
 // console.log(proxy.foo);
 // console.log("foo" in proxy);
 // console.log("foo" in myTarget);
- 
+
 // const myTarget = {};
 // const proxy = new Proxy(myTarget, {
 //   get(target, property, receiver) {
@@ -35,10 +34,55 @@ const myTarget = {};
 // console.log("foo" in proxy);
 // console.log("foo" in myTarget);
 
-const proxy = new Proxy(myTarget,{
-  defineProperty(target,property,descriptor,a){
-    console.log(target,property,descriptor,a,'defineProperty()')
-    return Reflect.defineProperty(...arguments)
-  }
-})
-Object.defineProperty(proxy,'foo',{value:'bar'})
+// const proxy = new Proxy(myTarget,{
+//   defineProperty(target,property,descriptor,a){
+//     console.log(target,property,descriptor,a,'defineProperty()')
+//     return Reflect.defineProperty(...arguments)
+//   }
+// })
+// Object.defineProperty(proxy,'foo',{value:'bar'})
+
+// getOwnPropertyDescriptor 返回四个参数configurable,enumerable,value,writable所对应的值
+myTarget.foo = "bar";
+// const proxy = new Proxy(myTarget,{
+//   getOwnPropertyDescriptor(target,property){
+//     console.log(target,property,'getOwnPropertyDescriptor()')
+//     return Reflect.getOwnPropertyDescriptor(...arguments)
+//   }
+// })
+// console.log(Object.getOwnPropertyDescriptor(proxy,'foo'))
+
+// deleteProperty
+// const proxy = new Proxy(myTarget,{
+//   deleteProperty(target,property){
+//     console.log(target,property,'deleteProperty()')
+
+//     return Reflect.deleteProperty(...arguments)
+//   }
+// })
+// delete proxy.foo
+// console.log(proxy,myTarget)
+// console.log(Object.deleteProperty(proxy,'foo'))
+
+// ownKeys
+//  const proxy = new Proxy(myTarget,{
+//   ownKeys (target){
+//       console.log(target,'ownKeys()')
+//       return Reflect.ownKeys(...arguments)
+//     }
+//   })
+//   console.log(Object.keys(proxy,{a:'1'}))
+//   Object.getOwnPropertyNames(proxy)
+//   Reflect.ownKeys(proxy)
+
+// getPrototypeOf 获取了类似Object 原型链中的内容
+// const proxy = new Proxy(myTarget, {
+//   getPrototypeOf(target) {
+//     console.log(target, "getPrototypeOf()");
+//     return Reflect.getPrototypeOf(...arguments);
+//   },
+// });
+
+// Object.getPrototypeOf(proxy);
+// console.log(Object.prototype.isPrototypeOf(proxy))
+// console.log(proxy instanceof Object)
