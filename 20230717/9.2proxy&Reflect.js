a5ec39f698192e9109cf2dc38d841bbc2abdd63f
王旭东 <xudong@DESKTOP-DK31HTA>
2023-07-24 17:08:07 +0800

    '0724'

--- 20230717/9.2proxy&Reflect.js
+++ 20230717/9.2proxy&Reflect.js
@@ -1,37 +1,44 @@
-<<<<<<< HEAD
+ 
 const myTarget = {};
-const proxy = new Proxy(myTarget, {
-  get(target, property, receiver) {
-    return Reflect.get(...arguments);
-  },
-  set(target, property, value, receiver) {
-    return Reflect.get(...arguments);
-  },
-  has(target, property) {
-    return Reflect.has(...arguments);
-  },
-});
-proxy.foo = "a"
-console.log( proxy.foo = "a");
-console.log(proxy.foo);
-console.log("foo" in proxy);
-console.log("foo" in myTarget);
-=======
-const myTarget = {};
-const proxy = new Proxy(myTarget, {
-  get(target, property, receiver) {
-    return Reflect.get(...arguments);
-  },
-  set(target, property, value, receiver) {
-    return Reflect.get(...arguments);
-  },
-  has(target, property) {
-    return Reflect.has(...arguments);
-  },
-});
-proxy.foo = "a"
-console.log( proxy.foo = "a");
-console.log(proxy.foo);
-console.log("foo" in proxy);
-console.log("foo" in myTarget);
->>>>>>> 6a0780528c0c1f3bf90b064d45ce15c0dfd88d06
+// const proxy = new Proxy(myTarget, {
+//   get(target, property, receiver) {
+//     return Reflect.get(...arguments);
+//   },
+//   set(target, property, value, receiver) {
+//     return Reflect.get(...arguments);
+//   },
+//   has(target, property) {
+//     return Reflect.has(...arguments);
+//   },
+// });
+// proxy.foo = "a"
+// console.log( proxy.foo = "a");
+// console.log(proxy.foo);
+// console.log("foo" in proxy);
+// console.log("foo" in myTarget);
+ 
+// const myTarget = {};
+// const proxy = new Proxy(myTarget, {
+//   get(target, property, receiver) {
+//     return Reflect.get(...arguments);
+//   },
+//   set(target, property, value, receiver) {
+//     return Reflect.get(...arguments);
+//   },
+//   has(target, property) {
+//     return Reflect.has(...arguments);
+//   },
+// });
+// proxy.foo = "a"
+// console.log( proxy.foo = "a");
+// console.log(proxy.foo);
+// console.log("foo" in proxy);
+// console.log("foo" in myTarget);
+
+const proxy = new Proxy(myTarget,{
+  defineProperty(target,property,descriptor,a){
+    console.log(target,property,descriptor,a,'defineProperty()')
+    return Reflect.defineProperty(...arguments)
+  }
+})
+Object.defineProperty(proxy,'foo',{value:'bar'})
