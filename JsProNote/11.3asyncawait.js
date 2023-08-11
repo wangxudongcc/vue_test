8438ccebaf377e1b9ad5537949e2b3a4720f1591
王旭东 <wxd5677@163.com>
2023-08-11 09:39:45 +0800

    '0811'

--- JsProNote/11.3asyncawait.js
+++ JsProNote/11.3asyncawait.js
@@ -1,9 +1,22 @@
-async function foo(){
-    console.log(2)
-    await null 
-    console.log(4)
-}
-console.log(1)
-foo()
-console.log(3)
+// async function foo(){
+//     console.log(2)
+//     await undefined 
+//     console.log(4)
+// }
+// console.log(1)
+// foo()
+// console.log(3)
 // 1 2 3 4
+
+async function foo(){
+    console.log(await Promise.resolve('foo'))
+}
+async function bar (){
+    console.log(await 'bar')
+}
+async function baz(){
+    console.log('null')
+}
+foo()
+bar()
+baz()
