7868653e1afc9b1f227030cb841c09ce163c7cd6
王旭东 <wxd5677@163.com>
2023-11-10 20:21:39 +0800

    逻辑清晰

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,31 +1,66 @@
+/* eslint-disable */
 <template>
   <div id="app">
-    <span @click="xxx">输出emit</span>
-    <el-button @click="showOrHide" :autofocus="autof" plain>显示隐藏</el-button>
-    <component-one v-if="!qufan" ref="templateOne" />
+    <el-form :model="personData" ref="personData" label-width="100px" class="demo-dynamic">
+      <el-form-item prop="name" label="姓名" :rules="[
+        { required: true, message: '请输入姓名', trigger: 'blur' } 
+      ]">
+        <el-input v-model="personData.name"></el-input>
+      </el-form-item>
+      <el-form-item v-for="(item, index) of personData.domains" :label="'手机号码' + index" :key="index"
+        :prop="'phone.' + index" 
+        :rules="{required: true, message: '手机号码不能为空', trigger: 'blur'}">
+        <el-input v-model="item.value"></el-input><el-button @click.prevent="removeDomain(item)">删除</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('personData')">提交</el-button>
+        <el-button @click="addDomain">新增</el-button>
+        <el-button @click="resetForm('personData')">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 <script>
-import ComponentOne from "./views/practise/components/componentOne.vue";
-import $eventBus from "./views/practise/components/eventBus";
+// import ComponentOne from "./views/practise/components/componentOne.vue"; 
+// import $eventBus from "./views/practise/components/eventBus";
 export default {
   data() {
     return {
-      qufan: false,
-      autof: false,
+      personData: {
+        name:'',
+        phone: [{value:''}]
+      }
     };
   },
   components: {
-    ComponentOne,
+    // ComponentOne,
   },
   methods: {
-    showOrHide() {
-      this.qufan = !this.qufan;
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
     },
-    xxx() {
-      console.log("执行xxx");
-      $eventBus.$emit("_zizujian", [{}, 666], "4");
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
     },
-  },
+    removeDomain(item) {
+      var index = this.personData.domains.indexOf(item)
+      if (index !== -1) {
+        this.personData.domains.splice(index, 1)
+      }
+    },
+    addDomain() {
+      this.personData.domains.push({
+        value: '',
+        key: Date.now()
+      });
+    }
+  }
 };
 </script>
--- vue_test/src/views/practise/components/componentOne.vue
+++ vue_test/src/views/practise/components/componentOne.vue
@@ -1,33 +1,22 @@
 <template>
   <div>
-    <div v-if="flag" ref="asdf">输出$on</div>
+
   </div>
 </template>
 
 <script>
-import $eventBus from "./eventBus";
+// import $eventBus from "./eventBus";
 
 export default {
   name: "ComponentOne",
   data() {
     return {
-      name: "111",
-      flag: true,
-      formData: {},
-    };
-  },
-  beforeDestroy() {
-    this._zizujian();
-  },
-  methods: {
-    _zizujian() {
-      let that = this;
-      $eventBus.$off("_zizujian");
-      $eventBus.$on("_zizujian", (rucan1, rucan2) => {
-        that.formData = rucan1;
-        that.name = rucan2;
-      })
+
     }
+  },
+  beforeDestroy() { },
+  methods: {
+
   }
 };
 </script>
