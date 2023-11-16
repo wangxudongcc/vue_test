a0d9ddaeafc3c6ced04b632ed76a425cd0820ca6
王旭东 <wxd5677@163.com>
2023-11-16 18:13:25 +0800

    数据调增

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,12 +1,11 @@
 
 <template>
   <div id="app">
-    <el-form  ref="nameList" label-width="80px" class="demo-dynamic"
+    <el-form ref="nameList" label-width="80px" class="demo-dynamic"
       style="border-bottom:1px solid black;margin:50px auto;width:80%">
-      <el-row>
+      <el-row v-for="(item, index) in nameList" :key="item.key">
         <el-col :span="6">
-          <el-form-item v-for="(item, index) in nameList" label="姓名" :key="item.key" :index="index"
-          :prop="item.value" >
+          <el-form-item  label="姓名">
             <el-input v-model="item.value"></el-input>
           </el-form-item>
         </el-col>
@@ -14,7 +13,7 @@
         <el-col :span="6">&nbsp;</el-col>
         <el-col :span="4">&nbsp;</el-col>
         <el-col :span="2">
-          <el-button type="text" @click.prevent="removeName(item)">删除</el-button>
+          <el-button type="text" @click.prevent="removeName(index)">删除</el-button>
         </el-col>
       </el-row>
       <el-form-item>
@@ -29,19 +28,16 @@
 
     <el-form ref="phoneNumberList" label-width="90px" class="demo-dynamic"
       style="border-bottom:1px solid;margin:0px auto;width:80%">
-      <el-row>
+      <el-row  v-for="(item, index) in phoneNumberList" :key="item.key">
         <el-col :span="7">
-          <el-form-item v-for="(item, index) in phoneNumberList" 
-          :label="'手机号码' + index" 
-          :key="item.key"
-          :prop="item.value">
+          <el-form-item  :label="nameListName+'手机号码' + index">
             <el-input v-model="item.value"></el-input> </el-form-item>
         </el-col>
         <el-col :span="5">&nbsp;</el-col>
         <el-col :span="6">&nbsp;</el-col>
         <el-col :span="4">&nbsp;</el-col>
         <el-col :span="2">
-          <el-button type="text" @click.prevent="removePhone(item)">删除</el-button>
+          <el-button type="text" @click.prevent="removePhone(index)">删除</el-button>
         </el-col>
       </el-row>
       <el-form-item>
@@ -54,20 +50,15 @@
       </el-form-item>
     </el-form>
 
-    <el-form  label-width="80px" class="demo-dynamic"
-    style="margin:24px auto;width:80%" >
-    <!-- :model="phoneNumberAmount" -->
+    <el-form label-width="80px" class="demo-dynamic" style="margin:24px auto;width:80%">
       <el-row>
-        <el-col :span="6">&nbsp;</el-col>
-        <el-col :span="10" >
-          <el-form-item
-          class="submitTable"
-          label-width="140px"
-          style="{ font-size: 10px ;color:'red'}"
-          label="'王旭东' + '留存手机号个数'" >
-            <el-input v-model="phoneNumberAmount"></el-input>
+        <el-col :span="10">
+          <el-form-item class="submitTable" label-width="140px" style="{ font-size: 10px ;color:'red'}"
+            :label="nameListName + '留存手机号个数'">
+            <el-input v-model="phoneAmount" readonly></el-input>
           </el-form-item>
         </el-col>
+        <el-col :span="6">&nbsp;</el-col>
         <el-col :span="2">&nbsp;</el-col>
         <el-col :span="6">&nbsp;</el-col>
       </el-row>
@@ -75,14 +66,13 @@
         <el-col :span="6">&nbsp;</el-col>
         <el-col :span="6">&nbsp;</el-col>
         <el-col :span="6">&nbsp;</el-col>
-        <el-col :span="6"> 
-        <el-form-item>
-        <el-button type="primary" @click="submitForm('phoneNumberAmount')">提 交</el-button>
-        </el-form-item>
-      </el-col>
+        <el-col :span="6">
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('personList')">提 交</el-button>
+          </el-form-item>
+        </el-col>
       </el-row>
     </el-form>
-
   </div>
 </template>
 <script>
@@ -91,13 +81,30 @@
 export default {
   data() {
     return {
-        nameList: [{value:''}],
-        phoneNumberList: [{value:''}],
-        phoneNumberAmount: '1'
+      nameList: [{ value: '' }],
+      phoneNumberList: [{ value: '' }],
+      
     };
   },
   components: {
     // ComponentOne,
+  },
+  computed: {
+    nameListName(){
+      let nameListName;
+      if(this.nameList.length==0){
+        nameListName=''
+      }else if(this.nameList[this.nameList.length - 1].value==''&&this.nameList.length>=2){
+        nameListName = this.nameList[this.nameList.length - 2].value
+      }else{
+        nameListName = this.nameList[this.nameList.length - 1].value
+      }
+      return nameListName
+    },
+    phoneAmount(){
+      return this.phoneNumberList.length
+    }
+
   },
   methods: {
     submitForm(formName) {
@@ -111,26 +118,21 @@ export default {
       });
     },
     resetForm(formName) {
-      if(formName=='nameList'){
-        this.nameList= [{value:''}]
-      }else if(formName=='phoneNumberList'){
-        this.phoneNumberList= [{value:''}]
+      if (formName == 'nameList') {
+        this.nameList = [{ value: '' }]
+      } else if (formName == 'phoneNumberList') {
+        this.phoneNumberList = [{ value: '' }]
       }
       // this.$refs[formName].resetFields();
     },
     // 移除手机号
-    removePhone(item) {
-      var index = this.phoneNumberList.indexOf(item)
-      if (index !== -1) {
-        this.phoneNumberList.splice(index, 1)
-      }
+    removePhone(index) {
+      this.phoneNumberList.splice(index, 1)
+
     },
     // 移除姓名
-    removeName(item) {
-      var index = this.nameList.indexOf(item)
-      if (index !== -1) {
-        this.nameList.splice(index, 1)
-      }
+    removeName(index) {
+      this.nameList.splice(index, 1)
     },
     //增加手机号
     addPhone() {
@@ -142,7 +144,7 @@ export default {
     //增加姓名
     addName() {
       this.nameList.push({
-        value:'',
+        value: '',
         key: Date.now()
       });
     }
@@ -152,7 +154,7 @@ export default {
 
 <style scoped>
 ::v-deep .el-form-item.submitTable>label {
-  font-size: 12px 
+  font-size: 12px
 }
 </style>
  
