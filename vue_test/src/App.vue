3b66c2910903ce399d74f71abc68f554cdfebbe8
王旭东 <wxd5677@163.com>
2023-11-20 19:01:51 +0800

    ''

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -5,7 +5,7 @@
       style="border-bottom:1px solid black;margin:50px auto;width:80%">
       <el-row v-for="(item, index) in nameList" :key="item.key">
         <el-col :span="6">
-          <el-form-item  label="姓名">
+          <el-form-item label="姓名">
             <el-input v-model="item.value"></el-input>
           </el-form-item>
         </el-col>
@@ -28,9 +28,9 @@
 
     <el-form ref="phoneNumberList" label-width="90px" class="demo-dynamic"
       style="border-bottom:1px solid;margin:0px auto;width:80%">
-      <el-row  v-for="(item, index) in phoneNumberList" :key="item.key">
+      <el-row v-for="(item, index) in phoneNumberList" :key="item.key">
         <el-col :span="7">
-          <el-form-item  :label="nameListName+'手机号码' + index">
+          <el-form-item label='手机号码'>
             <el-input v-model="item.value"></el-input> </el-form-item>
         </el-col>
         <el-col :span="5">&nbsp;</el-col>
@@ -53,9 +53,8 @@
     <el-form label-width="80px" class="demo-dynamic" style="margin:24px auto;width:80%">
       <el-row>
         <el-col :span="10">
-          <el-form-item class="submitTable" label-width="140px" style="{ font-size: 10px ;color:'red'}"
-            :label="nameListName + '留存手机号个数'">
-            <el-input v-model="phoneAmount" readonly></el-input>
+          <el-form-item class="submitTable" label-width="auto" style="{ font-size: 10px ;color:'red'}"
+            :label="labelJudge">
           </el-form-item>
         </el-col>
         <el-col :span="6">&nbsp;</el-col>
@@ -82,26 +81,43 @@ export default {
   data() {
     return {
       nameList: [{ value: '' }],
-      phoneNumberList: [{ value: '' }],
-      
+      phoneNumberList: [{ value: '18713102810' }],
+      personList: [
+       { name: 'wang', phoneNumber: 18713102810 }, 
+       { name: '董欢迎', phoneNumber: 15530717659 }, 
+       { name: 'wang', phoneNumber: 13102855460 },
+       { name: '王旭东', phoneNumber: 13292874554 }]
     };
   },
   components: {
     // ComponentOne,
   },
   computed: {
-    nameListName(){
-      let nameListName;
-      if(this.nameList.length==0){
-        nameListName=''
-      }else if(this.nameList[this.nameList.length - 1].value==''&&this.nameList.length>=2){
-        nameListName = this.nameList[this.nameList.length - 2].value
+    labelJudge() {
+      let labelInfo;
+      // this.personList.forEach((item, index) => {
+      //   console.log(item, index, 'item,index')
+      //   if (item.phoneNumber==this.phoneNumberList[this.phoneNumberList.length-1].value) {
+          
+      //     labelInfo= '手机号：' + this.personList[index].phoneNumber + '存在于本系统中,该用户名为' + item.name
+      //   } else if (item.name==this.nameList[this.nameList.length-1].value) {
+      //     labelInfo= item.name+'存在于本系统中,手机号为'+ item.phoneNumber 
+      //   }else{
+      //     labelInfo='需要填写手机号码或者姓名'
+      //   }
+      // })
+      let personItem = this.personList.find(item => item.phoneNumber == this.phoneNumberList[this.phoneNumberList.length-1].value);
+      let personNameItem = this.personList.find(item =>item.name == this.nameList[this.nameList.length-1].value);
+      if (personItem) {
+        labelInfo= '手机号：' + personItem.phoneNumber + '存在于本系统中,该用户名为' + personItem.name
+      }else if(personNameItem){
+        labelInfo= personNameItem.name+'存在于本系统中,手机号为'+ personNameItem.phoneNumber 
       }else{
-        nameListName = this.nameList[this.nameList.length - 1].value
-      }
-      return nameListName
+          labelInfo='需要填写手机号码或者姓名'
+        }
+      return labelInfo
     },
-    phoneAmount(){
+    phoneAmount() {
       return this.phoneNumberList.length
     }
 
