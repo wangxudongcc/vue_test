02b6ca5b9bb73f8973e4361e5442e3ef804c6a87
王旭东 <wxd5677@163.com>
2024-05-09 22:11:51 +0800

    feat:动画进入退出

---
+++ src/views/practise/components/test.vue
@@ -0,0 +1,15 @@
+<template>
+    <button>
+        <h1 v-show="isShow"></h1>
+    </button>
+</template>
+<script>
+export default {
+    name:'Test',
+    data(){
+        return{
+            isShow:true
+        }
+    }
+}
+</script>
